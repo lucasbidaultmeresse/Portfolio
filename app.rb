49c7fa9e17f2fc6f04594000842bc749280236fa
Lucas Bidault Meresse <2383383@crosemont.qc.ca>
2026-03-09 23:27:52 -0400

    Déploiement final : Routes et structure Sinatra OK

--- app.rb
+++ app.rb
@@ -1,28 +1,51 @@
 require 'sinatra'
 
-# On définit explicitement la racine du projet
+# Racine du projet
 set :root, File.dirname(__FILE__)
 
-# Configuration des dossiers (Syntaxe la plus sûre pour Render)
-set :views, File.join(settings.root, 'app', 'views', 'pages')
+# On remonte d'un dossier pour englober 'pages' et 'projets'
+set :views, File.join(settings.root, 'app', 'views')
 set :public_folder, File.join(settings.root, 'public')
 
-# Pour voir l'erreur réelle si ça replante
 set :show_exceptions, :after_handler
 
 helpers do
   def image_tag(path, options = {})
-    src = "/assets/images/#{path}" # On suppose que tes images sont dans public/assets/images
+    src = "/assets/images/#{path}"
     attrs = options.map { |k, v| "#{k}='#{v}'" }.join(" ")
     "<img src='#{src}' #{attrs}>"
   end
 end
 
+# --- PAGES PRINCIPALES ---
+
 get '/' do
-  # On vérifie si le fichier .html.erb existe, sinon on prend le .erb
-  if File.exist?(File.join(settings.views, "home.html.erb"))
-    erb :"home.html"
-  else
-    erb :home
-  end
+  erb :"pages/home"
+end
+
+get '/contacter' do
+  erb :"pages/contacter"
+end
+
+get '/projets' do
+  # Sinatra trouvera projets.html.erb ou projets.erb
+  erb :"pages/projets"
+end
+
+# --- PAGES DÉTAILS PROJETS ---
+
+get '/projets/busterblock' do
+  erb :"projets/busterblockapi"
+end
+
+get '/projets/greenpulse' do
+  erb :"projets/greenpulse"
+end
+
+get '/projets/kaypicphoto' do
+  erb :"projets/kaypicphoto"
+end
+
+get '/projets/recherchestage' do
+  erb :"projets/recherchestage"
 end
