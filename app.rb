ac065eb906725448366149636fc2c95c8b121f23
Lucas Bidault Meresse <2383383@crosemont.qc.ca>
2026-03-09 23:15:14 -0400

    update app.rb a la racine

--- app.rb
+++ app.rb
@@ -1,19 +1,24 @@
 require 'sinatra'
 
-# Configuration des dossiers
-set :views, File.join(settings.root, 'app/views/pages')
-set :public_folder, File.expand_path('../public', __FILE__)
+# On définit explicitement la racine du projet
+set :root, File.dirname(__FILE__)
 
-# Helper pour simuler Ruby on Rails
+# Configuration des dossiers (Syntaxe la plus sûre pour Render)
+set :views, File.join(settings.root, 'app', 'views', 'pages')
+set :public_folder, File.join(settings.root, 'public')
+
+# Pour voir l'erreur réelle si ça replante
+set :show_exceptions, :after_handler
+
 helpers do
   def image_tag(path, options = {})
-    # On gère le fait que tes images sont dans des sous-dossiers spécifiques
-    src = "/#{path}" 
+    src = "/assets/images/#{path}" # On suppose que tes images sont dans public/assets/images
     attrs = options.map { |k, v| "#{k}='#{v}'" }.join(" ")
     "<img src='#{src}' #{attrs}>"
   end
 end
 
 get '/' do
-  erb :home # Cherche app/views/pages/home.html.erb
+  # On s'assure de chercher le bon fichier (home.erb ou home.html.erb)
+  erb :home
 end
